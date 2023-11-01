c74788117ee6e129251d4578fc4763f0ba49d9cd
zhourusheng <zhourusheng@yunzhangfang.com>
2023-11-01 16:46:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <CesiumPage />
+</template>
+
+<script setup lang="ts">
+import CesiumPage from './components/CesiumPage/index.vue'
+</script>
+
+<style>
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/CesiumPage/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="cesium-viewer"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+
+onMounted(() => {
+  initMap()
+})
+
+function initMap() {
+  new Cesium.Viewer('cesium-viewer')
+}
+</script>
+
+<style>
+#cesium-viewer {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
